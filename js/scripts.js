0cc07ac469d9fc06b911efe825049e66a71f79be
xhl2022 <77228122@qq.com>
2023-11-30 04:26:28 +0900

    轮播图大小屏幕匹配修改

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,97 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+$(function(){//相当于onload
+    $(window).on("resize",function(){     // 监听屏幕事件
+        var windowWidth=$(window).width();     //获取屏幕尺寸
+        console.log(windowWidth);
+        var isSmallScreen = windowWidth < 768;// 返回布尔值
+        // 小屏幕追加img标签
+        if (isSmallScreen) {
+            // 找到需要操作的元素
+            $('#casCarousel .carousel-inner .carousel-item').each(function (i, item) {
+                var item = $(this);
+                // 当前自定义项目中 存储的图片路径
+                var imgSrc = isSmallScreen?item.data("image-xs"):item.data("image-lg")
+                console.log(imgSrc);
+                item.html('<a href="javascript:;" class="mobileImg"><img src="'+imgSrc+'" alt="..."></a>');
+            })
+        } else {
+            // 找到需要操作的元素
+            $('#casCarousel .carousel-inner .carousel-item').each(function (i, item) {
+                var item = $(this);
+                // 当前自定义项目中 存储的图片路径
+                var imgSrc = isSmallScreen?item.data("image-xs"):item.data("image-lg")
+                console.log(imgSrc);
+                item.html($('<a href="javascript:;" class="pcImg"></a>').css("background-image","url('"+imgSrc+"')"));
+            })
+        }
+    }).trigger("resize");
+})
+// $(function(){//相当于onload
+//     // 轮播图的响应式(大屏幕用背景图，小屏幕用img)
+//     // 根据屏幕宽度决定背景图形式还是图片
+//     function resize () {
+//         // 先获取屏幕宽度
+//         var windowWidth = $(window).width();
+//         // 判断当前屏幕大小
+//         var isSmallScreen = windowWidth < 768;// 返回布尔值
+//         // 找到需要操作的元素
+//         $('#casCarousel .carousel-inner .item').each(function (i, item) {
+//             var $item = $(item);
+//             var imgSrc = isSmallScreen?$item.data("image-xs"):$item.data("image-lg")
+//             // 大屏幕设置背景图片
+//             $item.css({
+//                 backgroundImage:'url(${imgSrc})'
+//             })
+//             // 小屏幕追加img标签
+//             if (isSmallScreen) {
+//                 $item.html('<img src=${imgSrc}>')
+//             } else {
+//                 $item.empty();
+//             }
+//         })
+//     } 
+//     // trigger（） 方法触发被选元素的指定事件类型。
+//     // 当调整浏览器窗口大小时，发生resize 事件。
+//     $ (window).on("resize", resize).trigger("resize");
+//     // 手机端的触碰事件
+//     var carousel = $(",carousel");
+//     var start, endx, moves;
+//     var startMove = 50;
+//     carousel.on("touchstart", function (e) {
+//         startX = e.originalEvent. touches [0].clientx;
+//     })
+//     carousel.on("touchmove", function (e) {
+//         endX = e.originalEvent.touches [0].clientX;
+//     })
+//     carousel. on("touchend", function (e) {
+//         moves = Math.abs(start - endx) - startMove;
+//         if (moves) {
+//             if (startX - endX > 0) {
+//                 $(this) .carousel ("next");
+//             } else if (startX - endX < 0) {
+//                 $(this).carousel("prev");
+//             }
+//         }
+//     })
+//     // 鼠标拖动事件
+//     var x1, x2, xmove
+//     carousel[0].onmousedown = function(e1){
+//         x1 = e1.clientx;
+//         carouse[0].onmousemove = function(e2){
+//         X2 = e2.CLientX;
+//         }
+//     }
+//     carousel[0].onmouseup= function(){
+//         xmove = Math. abs (x1 - x2) - startMove;
+//         if (xmove) {
+//             if (x1 - x2 > 0) {
+//                 $ (this).carousel("next");
+//             } else if (x1 - x2 < 0) {
+//                 $ (this).carousel("prev");
+//             }
+//         }
+//     }
+// })
